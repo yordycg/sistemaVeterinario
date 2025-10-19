8eeedaa44919f414d4206ab1547ff7bb1e1805e1
Yordy Carmona <yordy.carmona@virginiogomez.cl>
2025-10-19 17:11:44 -0300

    Feat(tratamientos): add server-side data annotations

--- Models/Tratamiento.cs
+++ Models/Tratamiento.cs
@@ -1,17 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
 
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations;
+
 namespace sistemaVeterinario.Models;
 
 public partial class Tratamiento
 {
     public int IdTratamiento { get; set; }
 
+    [Required(ErrorMessage = "Debe seleccionar una consulta.")]
     public int IdConsulta { get; set; }
 
+    [Required(ErrorMessage = "La descripción es obligatoria.")]
+    [StringLength(255, ErrorMessage = "La descripción no puede tener más de 255 caracteres.")]
     public string? Descripcion { get; set; }
 
+    [Required(ErrorMessage = "El medicamento es obligatorio.")]
+    [StringLength(100, ErrorMessage = "El medicamento no puede tener más de 100 caracteres.")]
     public string? Medicamento { get; set; }
 
+    [ValidateNever]
     public virtual Consulta IdConsultaNavigation { get; set; } = null!;
 }
