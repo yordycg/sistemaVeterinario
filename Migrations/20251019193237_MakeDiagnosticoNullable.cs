40a89ddefd8c633b81dac06701be418d78715798
Yordy Carmona <yordy.carmona@virginiogomez.cl>
2025-10-19 16:33:36 -0300

    Fix(db): allow nulls for diagnostico column

---
+++ Migrations/20251019193237_MakeDiagnosticoNullable.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace sistemaVeterinario.Migrations
+{
+    /// <inheritdoc />
+    public partial class MakeDiagnosticoNullable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "diagnostico",
+                table: "consultas",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldMaxLength: 1000);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "diagnostico",
+                table: "consultas",
+                type: "text",
+                maxLength: 1000,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+        }
+    }
+}
