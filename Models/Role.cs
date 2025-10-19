fb05b4a47dc79df0e410c6d0242c32f882be51ec
Yordy Carmona <yordy.carmona@virginiogomez.cl>
2025-10-19 11:47:21 -0300

    Feat(roles): add server-side data annotations

--- Models/Role.cs
+++ Models/Role.cs
@@ -1,12 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 
+using System.ComponentModel.DataAnnotations;
+
 namespace sistemaVeterinario.Models;
 
 public partial class Role
 {
     public int IdRol { get; set; }
 
+    [Required]
+    [StringLength(25)]
     public string NombreRol { get; set; } = null!;
 
     public virtual ICollection<Usuario> Usuarios { get; set; } = new List<Usuario>();
