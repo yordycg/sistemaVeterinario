4245b6cb0ede1f796e4834eef803a48831dcd444
Yordy Carmona <yordy.carmona@virginiogomez.cl>
2025-10-19 17:07:43 -0300

    Feat(tratamientos): add initial scaffold for tratamientos CRUD

---
+++ Controllers/TratamientoesController.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using sistemaVeterinario.Models;
+
+namespace sistemaVeterinario.Controllers
+{
+    public class TratamientoesController : Controller
+    {
+        private readonly SistemaVeterinarioContext _context;
+
+        public TratamientoesController(SistemaVeterinarioContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Tratamientoes
+        public async Task<IActionResult> Index()
+        {
+            var sistemaVeterinarioContext = _context.Tratamientos.Include(t => t.IdConsultaNavigation);
+            return View(await sistemaVeterinarioContext.ToListAsync());
+        }
+
+        // GET: Tratamientoes/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var tratamiento = await _context.Tratamientos
+                .Include(t => t.IdConsultaNavigation)
+                .FirstOrDefaultAsync(m => m.IdTratamiento == id);
+            if (tratamiento == null)
+            {
+                return NotFound();
+            }
+
+            return View(tratamiento);
+        }
+
+        // GET: Tratamientoes/Create
+        public IActionResult Create()
+        {
+            ViewData["IdConsulta"] = new SelectList(_context.Consultas, "IdConsulta", "Motivo");
+            return View();
+        }
+
+        // POST: Tratamientoes/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("IdTratamiento,IdConsulta,Descripcion,Medicamento")] Tratamiento tratamiento)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(tratamiento);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["IdConsulta"] = new SelectList(_context.Consultas, "IdConsulta", "Motivo", tratamiento.IdConsulta);
+            return View(tratamiento);
+        }
+
+        // GET: Tratamientoes/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var tratamiento = await _context.Tratamientos.FindAsync(id);
+            if (tratamiento == null)
+            {
+                return NotFound();
+            }
+            ViewData["IdConsulta"] = new SelectList(_context.Consultas, "IdConsulta", "Motivo", tratamiento.IdConsulta);
+            return View(tratamiento);
+        }
+
+        // POST: Tratamientoes/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("IdTratamiento,IdConsulta,Descripcion,Medicamento")] Tratamiento tratamiento)
+        {
+            if (id != tratamiento.IdTratamiento)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(tratamiento);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!TratamientoExists(tratamiento.IdTratamiento))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["IdConsulta"] = new SelectList(_context.Consultas, "IdConsulta", "Motivo", tratamiento.IdConsulta);
+            return View(tratamiento);
+        }
+
+        // GET: Tratamientoes/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var tratamiento = await _context.Tratamientos
+                .Include(t => t.IdConsultaNavigation)
+                .FirstOrDefaultAsync(m => m.IdTratamiento == id);
+            if (tratamiento == null)
+            {
+                return NotFound();
+            }
+
+            return View(tratamiento);
+        }
+
+        // POST: Tratamientoes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var tratamiento = await _context.Tratamientos.FindAsync(id);
+            if (tratamiento != null)
+            {
+                _context.Tratamientos.Remove(tratamiento);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool TratamientoExists(int id)
+        {
+            return _context.Tratamientos.Any(e => e.IdTratamiento == id);
+        }
+    }
+}
