23cd68b8b6b6e626cbce407e83a3de66c3af5b05
Yordy Carmona <yordy.carmona@virginiogomez.cl>
2025-11-02 21:09:33 -0300

    Feat(login): add login page structure and controller action

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -28,5 +28,13 @@ namespace sistemaVeterinario.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        /**
+         * Metodo que nos permite crear nuestro LOGIN.
+         */
+        public IActionResult Login()
+        {
+            return View();
+        }
     }
 }
