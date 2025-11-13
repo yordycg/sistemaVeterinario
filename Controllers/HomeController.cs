ecf67feddc7c8f3bad44f5c7983b04d304d427b8
Yordy Carmona <yordy.carmona@virginiogomez.cl>
2025-11-12 23:57:01 -0300

    Feat(login): add db context and validation to login

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using sistemaVeterinario.Models;
 
 namespace sistemaVeterinario.Controllers
@@ -7,10 +8,12 @@ namespace sistemaVeterinario.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly SistemaVeterinarioContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, SistemaVeterinarioContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
@@ -35,6 +38,46 @@ namespace sistemaVeterinario.Controllers
         public IActionResult Login()
         {
             return View();
+        }
+
+        /**
+         * 
+         */
+        [HttpPost]
+        public IActionResult Login(string email, string password)
+        {
+            // Validadiones.
+            // Verificar que los campos no esten vacios.
+            if (string.IsNullOrEmpty(email) || string.IsNullOrEmpty(password))
+            {
+                // ViewBag -> nos permite enviar datos desde el controller a la view.
+                ViewBag.Error = "Por favor, ingrese su correo y contraseña.";
+                return View(new {email, password});
+            }
+
+            // Hacer una consulta en la DB para verificar la existencia del usuario.
+            var user = _context.Usuarios.FirstOrDefault(u => u.Email == email && u.Password == password);
+
+            if (user == null)
+            {
+                ViewBag.Error = "Email y/o password incorrectos.";
+                return View();
+            }
+
+            /**
+             * Variable Session:
+             * - Variable temporal que se almacena mientras el usuario este loggeado.
+             * - Para utilizar estas variables se debe configurar en el Program.cs
+             * - SetString() -> permite guardar data en una variable session.
+             * - GetString() -> permite obtener data de una variable session.
+             */
+            HttpContext.Session.SetString("nombre", user.Nombre);
+
+            // TempData -> permite la comunicacion (data) entre controllers
+            // durara hasta que tenga el 'login' abierto...
+            TempData["nombre"] = HttpContext.Session.GetString("nombre");
+
+            return RedirectToAction("Index"); // redireccionar al localhost/Home/Index.
         }
     }
 }
