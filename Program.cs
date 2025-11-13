efcada15a48e0c17e011ebd0aeb2c8a79e593b57
Yordy Carmona <yordy.carmona@virginiogomez.cl>
2025-11-12 23:56:35 -0300

    Feat(app): enable and configure session managment

--- Program.cs
+++ Program.cs
@@ -8,7 +8,22 @@ builder.Services.AddDbContext<SistemaVeterinarioContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddControllersWithViews();
 
+/**
+ * Configurar las Variable Sessions.
+ * - Se debe hacer ANTES del 'builder.Build()'
+ */
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(15); // tiempo de expiracion de la session.
+    options.Cookie.HttpOnly = true; // 'cookie' solo accesible por el servidor (no puede acceder por JS).
+    options.Cookie.Name = "SistemaVeterinario.Session"; // nombre de la 'cookie' de session.
+    options.Cookie.IsEssential = true; // permite utilizar 'cookies' aunque el usuario no las acepte...
+});
+
 var app = builder.Build();
+
+// Habilitar el uso de sessions en la aplicacion.
+app.UseSession();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -27,7 +42,8 @@ app.MapStaticAssets();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
+    // Hacemos que la pagina de inico sea Login.
+    pattern: "{controller=Home}/{action=Login}/{id?}")
     .WithStaticAssets();
 
 
